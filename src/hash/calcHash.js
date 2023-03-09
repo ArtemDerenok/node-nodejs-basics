0a0d9dbe0f23961b4b3de447d2f6297d6f770c50
ArtemDerenok <derenokartem95@gmail.com>
2023-03-09 15:39:13 +0300

    feat: add hash calculate

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import { createHash } from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await fs.readFile(
+      "./src/hash/files/fileToCalculateHashFor.txt",
+      "utf-8"
+    );
+    const hash = createHash("sha256").update(data).digest("hex");
+    console.log(hash);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 await calculateHash();
